e893bcff04994b86d9be57aa115a7929ff307bec
Ru Tan <jutan2012@gmail.com>
2017-01-05 16:56:33 +0100

    Ajout structure html

--- parts/header.php
+++ parts/header.php
@@ -1,1 +1,14 @@
-put some header here
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="UTF-8">
+	<title>php-siteperso</title>
+</head>
+<body>
+<li><a href="../pages/contact.php?p=contact">contact</a></li>
+<li><a href="../pages/bio.php?page=bio">bio</a></li>
+
+<?php 
+include 'menu.php' ?>
+	
